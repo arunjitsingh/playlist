bec311d1e599e2a1d2f3047d180738d3c4da3a61
Arunjit Singh <arunjit@me.com>
2012-06-24 19:03:56 +0530

    Don't close the playlist class defs scope
    The file that implements them and creates global *objects* can
    close their scope.

--- app/js/playlist.js
+++ app/js/playlist.js
@@ -1,41 +1,35 @@
-;(function($, _) {
-  var G = this;  // global object
+var Song = Backbone.Model.extend({
+  defaults: function() {
+    return {
+      title: '',    // {string} Title
+      artist: '',   // {string} Artist
+      album: '',    // {string} Album
+      artwork: '',  // {string} Artwork URL
+      year: 0       // {number} Year of release
+    };
+  }
+});
 
 
-  G.Song = Backbone.Model.extend({
-    defaults: function() {
-      return {
-        title: '',    // {string} Title
-        artist: '',   // {string} Artist
-        album: '',    // {string} Album
-        artwork: '',  // {string} Artwork URL
-        year: 0       // {number} Year of release
-      };
-    }
-  });
+var SongView = Backbone.View.extend({
 
+  className: 'song',
 
-  G.SongView = Backbone.View.extend({
-    
-    className: 'song',
-    
-    template: Handlebars.compile($('#song-tpl').html()),
-    
-    initialize: function() {
-      _.bindAll(this, 'render');
-      this.model.bind('change', this.render);
-    },
-    
-    render: function() {
-      $(this.el).html(this.template(this.model.toJSON()));
-      return this;
-    }
-  });
+  template: Handlebars.compile($('#song-tpl').html()),
+
+  initialize: function() {
+    _.bindAll(this, 'render');
+    this.model.bind('change', this.render);
+  },
+
+  render: function() {
+    $(this.el).html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
 
 
 
-  G.Playlist = Backbone.Collection.extend({
-    model: G.Song
-  });
-
-}).call(this, jQuery, _);
+var Playlist = Backbone.Collection.extend({
+  model: G.Song
+});
